3e12e2c428957e5a85be3c6a0dcca48fae8cdf4e
ghaneeyah <sakaganiyatt183@gmail.com>
2024-06-27 11:37:50 +0100

    first batch

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -77,7 +77,7 @@ exports.initiatePayment = async (req, res) => {
             tx_ref: orderId,
             amount,
             currency,
-            redirect_url: "http://localhost:5173/thanks",
+            redirect_url: "https://technotronix-frontend-wheat.vercel.app//thanks",
             customer: {
                 email: user.email,
                 name: `${user.firstName} ${user.lastName}`,
